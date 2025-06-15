58cb7a9bd0462998d6e3f814410ee7cbeb028543
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2025-06-15 14:11:03 +0300

    Add project files.

---
+++ API/NestLibraryNetCore.Api/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace NestLibraryNetCore.Api.Extensions
+{
+    public static class ElasticSearchExtensions
+    {
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
+            var settings = new ConnectionSettings(pool);
+            var client = new ElasticClient(settings);
+            services.AddSingleton(client);
+        }
+    }
+}
